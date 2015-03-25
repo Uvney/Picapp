d58c1bd135904d929368473f50440e54ef9619b9
vumai <vu.mai@myblee.info>
2015-03-25 13:29:14 +0700

    new project

---
+++ Picapp/Picapp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Picapp
+//
+//  Created by Vu Mai on 3/25/15.
+//  Copyright (c) 2015 VuMai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
